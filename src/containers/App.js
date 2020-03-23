ced9d62e8c8bed3619432c3b3e9c3f68894d8a08
Ger121190 <german.cabrera@connectis-ict.com.ar>
2020-03-22 22:13:35 -0300

    Refactor de folders del proyecto.

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import '../containers/App.css'
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value });
+    }
+
+    render() {
+        const { robots, searchField } = this.state;
+        const filteredRobots = robots
+            .filter(robot => {
+                return robot.name
+                    .toLowerCase()
+                    .includes(searchField.toLowerCase());
+            });
+        return !robots.length ?
+            <h1 className="tc">LOADING...</h1> :
+            <div className="tc">
+                <h1 className="f2">ROBOFRIENDS</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
+            </div>
+    }
+}
+
+export default App;
